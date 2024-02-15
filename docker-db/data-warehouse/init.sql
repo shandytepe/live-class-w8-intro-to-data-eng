58edeac337c7f3199a8ff7e3425779c5dd71ea6d
shandytepe <shandy@pacmannai.com>
2024-02-15 11:18:00 +0700

    update schema for data warehouse

--- docker-db/data-warehouse/init.sql
+++ docker-db/data-warehouse/init.sql
@@ -2,10 +2,13 @@ CREATE TABLE IF NOT EXISTS hotel_analysis_table(
     analysis_id SERIAL NOT NULL PRIMARY KEY,
     reservation_id integer,
     full_name varchar,
+    email varchar,
+    domain_email varchar,
     reservation_date timestamp,
     start_date timestamp,
     end_date timestamp,
     total_price numeric,
+    currency varchar,
     provider varchar,
     payment_status varchar,
     created_at timestamp NOT NULL DEFAULT NOW()
